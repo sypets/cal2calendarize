f521b89d62ce9765448b2c7965724c2de7884495
Sybille Peters <sypets@gmx.de>
2021-05-29 06:44:58 +0200

    Add TYPO3 comment to classes

--- Classes/Repository/PluginRepository.php
+++ Classes/Repository/PluginRepository.php
@@ -2,6 +2,19 @@
 
 namespace Sypets\Cal2calendarize\Repository;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryHelper;
@@ -11,7 +24,6 @@ use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Log\Logger;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Sypets\Cal2calendarize\Configuration\PluginFlexform;
-
 
 class PluginRepository
 {
